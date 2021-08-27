ebda1f1fd978fc7a45984069ec491aeed951bf17
Andrew Okoye <andrewinsoul@gmail.com>
2021-08-27 04:32:27 +0100

    added another origin

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', ['http://localhost:3000']);
+  res.header('Access-Control-Allow-Origin', [
+    'http://localhost:3000',
+    'https://sort-cat.herokuapp.com'
+  ]);
   res.header(
     'Access-Control-Allow-Methods',
     'GET'
