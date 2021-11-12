592dec9cc9cd110a4575087e1c0bf6133e524e45
Andrew Okoye <andrewinsoul@gmail.com>
2021-11-12 02:48:40 +0100

    implement graceful shutdown  of http server

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import 'babel-polyfill';
-import 'babel-core/register';
-import express from 'express';
-import dotenv from 'dotenv';
-import router from './routes';
-import CatApiController from './controllers';
+import "babel-polyfill";
+import "babel-core/register";
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes";
+import CatApiController from "./controllers";
 
 dotenv.config();
 
@@ -11,27 +11,37 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header(
-    'Access-Control-Allow-Origin',
-    process.env.NODE_ENV === 'production'
-      ? 'https://sort-cat.herokuapp.com'
-      : 'http://localhost:3000'
+    "Access-Control-Allow-Origin",
+    process.env.NODE_ENV === "production"
+      ? "https://sort-cat.herokuapp.com"
+      : "http://localhost:3000"
   );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET'
-  );
+  res.header("Access-Control-Allow-Methods", "GET");
   next();
 });
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   CatApiController.ping(req, res);
 });
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
   CatApiController.handleInvalidRoute(req, res);
 });
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 8001;
 
-app.listen(port, () => console.log(`server live on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`server live on port ${port}`)
+);
+
+console.log(`The process ID of this process is: ${process.pid}`);
+
+process.on("SIGTERM", () => {
+  console.info("SIGTERM signal received");
+  console.log("Shutting Down Http Server...");
+  server.close(() => {
+    console.log("Http Server Closed.");
+    process.exit(0);
+  });
+});
 export default app;
